f8c790259fae3f8949f02e2168850fe2c5358469
07arif <ai.arif0711@gmail.com>
2022-08-25 23:50:37 +0600

    let const added

---
+++ let-const.js
@@ -0,0 +1,12 @@
+//breakup with var
+//no more use of var
+//ler : let it to reassign
+// const: do not allow it to reassign
+
+let money = 34;
+money = 50;
+console.log(money);
+
+const bird = 'jaan pakhi';
+// bird = 'moyna pakhi';
+console.log(bird);
