0cb58e4b58995e1729927e5739c5eae937938637
07arif <ai.arif0711@gmail.com>
2022-08-25 23:41:55 +0600

    js  added

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,5 @@
 
-//getElementById and getElementByClassName Uses.
+//## getElementById and getElementByClassName Uses.
 
 document.getElementById('apply-background').addEventListener('click', function () {
     // console.log('apply background');
@@ -11,9 +11,26 @@ document.getElementById('apply-background').addEventListener('click', function (
     }
 });
 
+//## specific akta element  a stye use kora
 document.getElementById('center-btn').addEventListener('click', function () {
     // console.log('centering');
     const third = document.getElementById('third-friend');
     third.style.textAlign = 'center';
+
+})
+
+//## add an element using evet listner
+document.getElementById('add-friend'), addEventListener('click', function () {
+    // console.log('adding');
+    const friendContainer = document.getElementById('friends');
+    const friendadd = document.createElement('div');
+    friendadd.classList.add('friend');
+
+    friendadd.innerHTML = `
+<h3 class="friend-name">New Friend</h3>
+ <p>Someting new Added</p>
+`;
+
+    friendContainer.appendChild(friendadd);
 
 })
